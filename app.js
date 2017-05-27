678232c356761be0870bdd19b24356840930abac
Brandon Hogan <me@brandonhogan.com>
2017-05-27 16:34:32 -0400

    added a bootstrap and bower

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 
-var port = 8888;
+var port = 5001;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res) {
+    res.send('Hello world');
+});
+
+app.get('/books', function(req, res) {
+    res.send('Hello books');
+});
 
 app.listen(port, function(err) {
     console.log('running server on port ' + port);
