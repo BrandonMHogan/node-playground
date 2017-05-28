116e96015327092e1bceb812111338e20c00a3c9
Brandon Hogan <me@brandonhogan.com>
2017-05-27 21:30:58 -0400

    setup nodemon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var port = 5001;
+var port = process.env.PORT || 5001;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
