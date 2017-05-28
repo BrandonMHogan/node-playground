055a17d347a2bbda20ddd16aea6f140107ebfe7e
Brandon Hogan <me@brandonhogan.com>
2017-05-27 21:04:39 -0400

    setup bower and gulp dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -12,3 +12,16 @@ gulp.task('style', function () {
             verbose: true
         }));
 });
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+});
