07e3c97522d5427f2ea46d823535373767e4e408
Brandon Hogan <me@brandonhogan.com>
2017-06-01 14:10:37 -0400

    mongo and mongoose setup

--- api/app.js
+++ api/app.js
@@ -1,12 +1,34 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var book = require('./models/bookModel');
+
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req,res) {
+        book.find(function(err, books) {
+            if(err) {
+                res.status(500).send(err);
+            }
+            else {
+                res.json(books);
+            }
+        });
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('Welcome to my API');
 });
 
 app.listen(port, function() {
-    console.log('RUnning on PORT: ' + port);
+    console.log('Running on PORT: ' + port);
 });
