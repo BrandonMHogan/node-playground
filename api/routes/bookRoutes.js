47e9b606cfd733f91711f36d28081bfe327889d5
Brandon Hogan <me@brandonhogan.com>
2017-06-02 14:45:56 -0400

    added put and patch for book

--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -34,14 +34,55 @@ var routes = function(Book) {
         });
 
     // Route that takes the param and passes it into the book model
+    router.use('/:bookId', function(req, res, next) {
+        Book.findById(req.params.bookId, function(err, book) {
+            if(err) {
+                res.status(500).send(err);
+            }
+            else if(book) {
+                req.book = book;
+                next();
+            }
+            else {
+                res.status(404).send('No book found');
+            }
+        });
+    });
     router.route('/:bookId')
         .get(function(req,res) {
-            Book.findById(req.params.bookId, function(err, books) {
+            res.json(req.book);
+        })
+        .put(function(req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            
+            req.book.save(function(err) {
                 if(err) {
                     res.status(500).send(err);
                 }
                 else {
-                    res.json(books);
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function(req, res) {
+
+            // Deletes the id from the body
+            if(req.body._id)
+                delete req.body._id;
+
+            for(var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function(err) {
+                if(err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(req.book);
                 }
             });
         });
