54d5dd90b14c9a0d0536b0d0c55a4fce507bedc5
Brandon Hogan <me@brandonhogan.com>
2017-06-02 11:51:17 -0400

    POST now working, and has been moved into its own routes file

--- api/app.js
+++ api/app.js
@@ -1,52 +1,21 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
-var book = require('./models/bookModel');
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/books')
-    .get(function(req,res) {
+bookRouter = require('./routes/bookRoutes')(Book);
 
-        // The query object which is passed into the book model
-        var query = {};
-
-        // If you limit the query object, you can prevent 
-        // searches on things you dont want
-        if(req.query.genre) {
-            query.genre = req.query.genre;
-        }
-
-        book.find(query, function(err, books) {
-            if(err) {
-                res.status(500).send(err);
-            }
-            else {
-                res.json(books);
-            }
-        });
-    });
-
-// Route that takes the param and passes it into the book model
-bookRouter.route('/books/:bookId')
-    .get(function(req,res) {
-            book.findById(req.params.bookId, function(err, books) {
-                if(err) {
-                    res.status(500).send(err);
-                }
-                else {
-                    res.json(books);
-                }
-            });
-        });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('Welcome to my API');
---
+++ api/routes/bookRoutes.js
@@ -0,0 +1,50 @@
+var express = require('express');
+
+var routes = function(Book) {
+    var router = express.Router();
+
+    router.route('/')
+        .post(function(req, res){
+            var myBook = new Book(req.body);
+
+            myBook.save();
+            res.status(201).send(myBook);
+        })
+        .get(function(req,res) {
+
+            // The query object which is passed into the book model
+            var query = {};
+
+            // If you limit the query object, you can prevent 
+            // searches on things you dont want
+            if(req.query.genre) {
+                query.genre = req.query.genre;
+            }
+
+            Book.find(query, function(err, books) {
+                if(err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(books);
+                }
+            });
+        });
+
+    // Route that takes the param and passes it into the book model
+    router.route('/:bookId')
+        .get(function(req,res) {
+            Book.findById(req.params.bookId, function(err, books) {
+                if(err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json(books);
+                }
+            });
+        });
+
+    return router;
+};
+
+module.exports = routes;
